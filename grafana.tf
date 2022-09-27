6916f3bd006f3014cbc80fc06b0797534657fcd2
Linus Norton <linusnorton@gmail.com>
2022-09-27 12:34:02 +0100

    update grafana location

--- grafana.tf
+++ grafana.tf
@@ -2,7 +2,7 @@
 resource "azurerm_dashboard_grafana" "dashboard-grafana" {
   name                              = "${var.product}-grafana-${var.env}"
   resource_group_name               = azurerm_resource_group.rg.name
-  location                          = 'westeurope'
+  location                          = "westeurope"
 
   identity {
     type = "SystemAssigned"
