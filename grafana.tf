d608dcb7f2bb34d3c9d0eb55afd2e4df902ea396
Linus Norton <linusnorton@gmail.com>
2022-09-27 14:33:05 +0100

    Add roles for grafana

--- grafana.tf
+++ grafana.tf
@@ -10,3 +10,23 @@ resource "azurerm_dashboard_grafana" "dashboard-grafana" {
 
   tags = var.common_tags
 }
+
+data "azuread_group" "editors" {
+  display_name = "DTS CFT Developers"
+}
+
+resource "azurerm_role_assignment" "editors" {
+  scope                = azurerm_dashboard_grafana.dashboard-grafana.id
+  role_definition_name = "Grafana Editor"
+  principal_id         = data.azuread_group.editors.object_id
+}
+
+data "azuread_group" "admins" {
+  display_name = "DTS CFT Software Engineering"
+}
+
+resource "azurerm_role_assignment" "admin" {
+  scope                = azurerm_dashboard_grafana.dashboard-grafana.id
+  role_definition_name = "Grafana Admin"
+  principal_id         = data.azuread_group.admins.object_id
+}
