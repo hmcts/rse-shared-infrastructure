3e335f0d4536d08079df52a2481a3f4bb7ab27a4
Linus Norton <linusnorton@gmail.com>
2022-10-11 09:54:49 +0100

    whitelist all grafana IPs

--- postgres.tf
+++ postgres.tf
@@ -18,7 +18,7 @@ module "database" {
 
 resource "azurerm_postgresql_firewall_rule" "grafana" {
   for_each            = azurerm_dashboard_grafana.dashboard-grafana.outbound_ip
-  name                = "grafana" + each.key
+  name                = "grafana${each.key}"
   resource_group_name = module.database.resource_group_name
   server_name         = module.database.name
   start_ip_address    = each.value
