4e0deca4d0562a16cc23a7e7094219d07c3e78cd
Jason Paige <j@jasonpaige.co.uk>
2023-08-08 15:52:41 +0100

    Move App Insights to UK South

--- variables.tf
+++ variables.tf
@@ -21,11 +21,6 @@ variable "jenkins_AAD_objectId" {
   description = "(Required) The Azure AD object ID of a user, service principal or security group in the Azure Active Directory tenant for the vault. The object ID must be unique for the list of access policies."
 }
 
-variable "appinsights_location" {
-  default     = "West Europe"
-  description = "Location for Application Insights"
-}
-
 variable "subscription" {}
 
 variable "aks_subscription_id" {}
