e9275ddf4384c7490de707538abc0a6d3653230f
engineeringstuff <292243+engineeringstuff@users.noreply.github.com>
2024-07-01 15:13:34 +0100

    Comment out budget-alert

--- budget-alert.tf
+++ budget-alert.tf
@@ -1,32 +1,32 @@
-resource "azurerm_consumption_budget_resource_group" "grafana-budget-alert" {
-  count             = var.dashboard_count
-  name              = "grafana-budget-alert"
-  resource_group_id = azurerm_resource_group.rg.id
-
-  amount     = 500
-  time_grain = "Monthly"
-
-  time_period {
-    start_date = "2023-09-01T00:00:00Z"
-  }
-
-  #filter {
-  #  dimension {
-  #    name = "ResourceId"
-  #    values = [
-  #      azurerm_dashboard_grafana.dashboard-grafana[0].id,
-  #    ]
-  #  }
-  #}
-
-  notification {
-    enabled        = true
-    threshold      = 90.0
-    operator       = "GreaterThanOrEqualTo"
-    threshold_type = "Forecasted"
-
-    contact_groups = [
-      module.alert-action-group.action_group_name
-    ]
-  }
-}
+#resource "azurerm_consumption_budget_resource_group" "grafana-budget-alert" {
+#  count             = var.dashboard_count
+#  name              = "grafana-budget-alert"
+#  resource_group_id = azurerm_resource_group.rg.id
+#
+#  amount     = 500
+#  time_grain = "Monthly"
+#
+#  time_period {
+#    start_date = "2023-09-01T00:00:00Z"
+#  }
+#
+#  filter {
+#    dimension {
+#      name = "ResourceId"
+#      values = [
+#        azurerm_dashboard_grafana.dashboard-grafana[0].id,
+#      ]
+#    }
+#  }
+#
+#  notification {
+#    enabled        = true
+#    threshold      = 90.0
+#    operator       = "GreaterThanOrEqualTo"
+#    threshold_type = "Forecasted"
+#
+#    contact_groups = [
+#      module.alert-action-group.action_group_name
+#    ]
+#  }
+#}
